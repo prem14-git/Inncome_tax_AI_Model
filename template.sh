621d327b31dd0f162cf2412dc1fa38c1846e9244
prem14-git <premkulkarni1407@gmail.com>
2025-09-06 17:28:51 +0530

    folder structure

---
+++ template.sh
@@ -0,0 +1,17 @@
+# Directories
+
+mkdir -p src
+mkdir -p research 
+
+# files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+# environment variable file 
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
