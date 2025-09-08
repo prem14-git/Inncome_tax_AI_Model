2e65af675e67e40f6e2b6c92ab31d5b1ce22fa6a
prem14-git <premkulkarni1407@gmail.com>
2025-09-08 19:39:29 +0530

    testHB

--- app.py
+++ app.py
@@ -696,4 +696,5 @@ def smtp_diagnostics():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port= 8080, debug= True)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port, debug=True)
